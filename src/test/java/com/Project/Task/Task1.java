7d474a7c1abb66b4cd01fd7a3f7b2517680a2638
satheeshchandra4018 <satheeshchandra.suru@gmail.com>
2023-10-26 09:29:30 +0530

    First code

---
+++ src/test/java/com/Project/Task/Task1.java
@@ -0,0 +1,16 @@
+package com.Project.Task;
+
+public class Task1 {
+	private void method1() {
+		System.out.println("1");
+	}
+	private void method2() {
+		System.out.println("2");
+
+	}
+	private void method3() {
+		System.out.println("3");
+
+	}
+
+}
